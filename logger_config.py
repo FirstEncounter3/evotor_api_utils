2c727fa09a49c3220284738f41cc519ab8444ffe
FirstEncounter3 <needenergy27@gmail.com>
2024-10-21 13:46:03 +0700

    add logging config

---
+++ logger_config.py
@@ -0,0 +1,39 @@
+import os
+import logging.config
+
+LOG_DIR = "logs"
+LOG_FILE = "get_stores_and_devices.log"
+
+LOGGING_CONFIG = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "default": {
+            "format": "%(asctime)s - %(levelname)s - %(message)s - %(name)s",
+            "datefmt": "%Y-%m-%d %H:%M:%S",
+        }
+    },
+    "handlers": {
+        "file": {
+            "class": "logging.FileHandler",
+            "filename": os.path.join(LOG_DIR, LOG_FILE),
+            "formatter": "default",
+        },
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "default",
+        },
+    },
+    "loggers": {
+        "": {
+            "handlers": ["file", "console"],
+            "level": "INFO",
+            "propagate": True,
+        }
+    },
+}
+
+if not os.path.exists(LOG_DIR):
+    os.makedirs(LOG_DIR)
+
+logging.config.dictConfig(LOGGING_CONFIG)
